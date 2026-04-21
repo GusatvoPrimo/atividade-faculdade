5989d16a3821eafac6b7321cdb0fe5bac103d4b0
GusatvoPrimo <148510879+GusatvoPrimo@users.noreply.github.com>
2026-04-21 19:59:43 -0300

    Create atvd5.c

---
+++ atvd5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a, b, c, temp;
+
+    printf("Digite tres numeros inteiros:\n");
+    scanf("%d %d %d", &a, &b, &c);
+
+   
+    if (a < b) {
+        temp = a;
+        a = b;
+        b = temp;
+    }
+
+    if (a < c) {
+        temp = a;
+        a = c;
+        c = temp;
+    }
+
+    if (b < c) {
+        temp = b;
+        b = c;
+        c = temp;
+    }
+
+    printf("Ordem decrescente: %d %d %d\n", a, b, c);
+
+    system("pause");
+    return 0;
+}
